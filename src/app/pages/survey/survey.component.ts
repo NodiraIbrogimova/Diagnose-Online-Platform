7ca93d9194ab2b89cd6d30ee1743f90d743ae01c
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2021-11-07 19:26:33 +0500

    feat & style: add survey page, style components and green buttons

--- src/app/pages/survey/survey.component.ts
+++ src/app/pages/survey/survey.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SurveyComponent implements OnInit {
 
-  constructor() { }
+  baseUrl = "assets/images/tf-";
+  images: Array<String> = [];
+  constructor() {
+    for (let index = 1; index < 8; index++) {
+      this.images.push(this.baseUrl + String(index) + ".webp");
+    }
+  }
 
   ngOnInit(): void {
+
   }
 
 }
